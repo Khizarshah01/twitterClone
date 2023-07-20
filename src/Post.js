8c6e756714d813dd131dfd7a7a95a3383bb160c7
Khizarshah01 <khizar.shah010@gmail.com>
2023-07-20 12:18:35 +0530

    add component in post

--- src/Post.js
+++ src/Post.js
@@ -1,9 +1,44 @@
 import React from 'react'
 import "./Post.css"
-function Post() {
+import { Avatar } from '@mui/material'
+import { ChatBubbleOutline, FavoriteBorder, Publish, Repeat, VerifiedUser } from '@mui/icons-material'
+function Post({
+  displayName,
+  username,
+  verified,
+  text,
+  image,
+  avatar
+
+}) {
   return (
-    <div>
-        
+    <div className='post'>
+        <div className='post__avatar'>
+
+          <Avatar src='https://media.comicbook.com/2020/12/naruto-1249229.jpeg?auto=webp'></Avatar>
+        </div>
+        <div className='post__body'>
+          <div className='post__header'>
+            <div className='post__headerText'>
+              <h3>
+                khizar shah{" "}
+                <span>
+                <VerifiedUser className="post__badge"></VerifiedUser>
+             </span>
+              </h3>
+            </div>
+            <div className='post__description'>
+              <p>lore fewere  wr w wer e rw r</p>
+            </div>
+          </div>
+          <img src='https://wallpapers-clan.com/wp-content/uploads/2023/01/naruto-gif-pfp-1.gif' alt='hory'/>
+         <div className='post__footer'>
+          <ChatBubbleOutline fontSize='small'/>
+          <Repeat fontSize='small' />
+          <FavoriteBorder fontSize='small'/>
+          <Publish fontSize='small'/>
+         </div>
+         </div>
     </div>
   )
 }
